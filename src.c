9db0966168e5ca06da4acc1e17cd33f1fe061a2a
reudej <reuel@email.cz>
2025-07-06 02:06:41 +0200

    Add SQL injection protection (unfinished).

--- src.c
+++ src.c
@@ -19,6 +19,7 @@
 #define MAX_SESSIONS 1000
 #define CSRF_TOKEN_SIZE 32
 #define SESSION_ID_SIZE 16
+#define MAX_IDENTIFIER_LEN 63
 
 // Struktura pro šablonový systém
 typedef struct {
@@ -313,20 +314,70 @@ int validate_csrf_token(session_t *session, const char *token) {
 // DATABÁZOVÝ SYSTÉM
 // =============================================================================
 
+char* sanitize_sql_value(const char *input, const int single_or_double_quote) {
+    if (!input) return strdup("");
+
+    size_t len = strlen(input);
+    // Nejhorší případ: každý znak je uvozovka a je zdvojena => 2× velikost + 1
+    char *sanitized = malloc(len * 2 + 1);
+    if (!sanitized) return NULL;
+
+    char quote;
+    if (single_or_double_quote) quote = '\'';
+    else quote = '"'; 
+    char *dst = sanitized;
+    for (const char *src = input; *src; src++) {
+        if (*src == quote) {
+            *dst++ = quote;
+            *dst++ = quote;
+        } else {
+            *dst++ = *src;
+        }
+    }
+    *dst = '\0';
+    return sanitized;
+}
+
+// Zavést funkci: int is_valid_where_clause
+
+int is_valid_sql_id(const char *s) {
+    if (!s || strlen(s) == 0 || strlen(s) > MAX_IDENTIFIER_LEN)
+        return 0;
+
+    if (!isalpha(s[0]) && s[0] != '_')
+        return 0;
+
+    for (int i = 1; s[i] != '\0'; i++) {
+        if (!isalnum(s[i]) && s[i] != '_')
+            return 0;
+    }
+
+    return 1;
+}
+
+signed int validate_sql_ids(char **identifiers, int count) {
+    for (int i=0;i < count;i++) {
+        if (!is_valid_sql_id(identifiers[i])) {
+            return i;  // nevalidní název na indexu i
+        }
+    }
+    return -1;  // vše OK
+}
+
 int init_database(const char *conninfo) {
     pg_conn = PQconnectdb(conninfo);
     if (PQstatus(pg_conn) != CONNECTION_OK) {
-        kore_log(LOG_ERR, "PostgreSQL připojení selhalo: %s", PQerrorMessage(pg_conn));
+        kore_log(LOG_ERR, "PostgreSQL connnection failed: %s", PQerrorMessage(pg_conn));
         PQfinish(pg_conn);
         return 0;
     }
     return 1;
 }
 
-int create_table(const char *query) {
+int sql_query_exc(const char *query) {
     PGresult *res = PQexec(pg_conn, query);
     if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-        kore_log(LOG_ERR, "Chyba při vytváření tabulky: %s", PQerrorMessage(pg_conn));
+        kore_log(LOG_ERR, "Error while executing command: %s", PQerrorMessage(pg_conn));
         PQclear(res);
         return 0;
     }
@@ -334,30 +385,59 @@ int create_table(const char *query) {
     return 1;
 }
 
-int model_insert(const char *table, const char **fields, const char **values, int count) {
-    char fields_buf[1024] = "";
-    char values_buf[1024] = "";
+int sql_insert(const char *table, const char **fields, const char **values, int count) {
+    if (count <= 0) return 0;
 
+    int invalid_id_i = validate_sql_ids(fields, count);
+    if (invalid_id_i != -1) {
+        kore_log(LOG_ERR, "SQL insert failed: invalid field id on index %d: %s", invalid_id_i, fields[invalid_id_i]);
+        return 0;
+    }
+
+    char fields_buf[1024] = "";
     for (int i = 0; i < count; i++) {
         strcat(fields_buf, fields[i]);
-        strcat(values_buf, "'");
-        strcat(values_buf, values[i]);
-        strcat(values_buf, "'");
         if (i < count - 1) {
             strcat(fields_buf, ", ");
-            strcat(values_buf, ", ");
         }
     }
 
-    char query[2048];
-    snprintf(query, sizeof(query), "INSERT INTO %s (%s) VALUES (%s);", table, fields_buf, values_buf);
+    // Vytvoříme parametrizovaný dotaz s $1, $2, ... dle počtu polí
+    char query[2048] = "";
+    snprintf(query, sizeof(query), "INSERT INTO %s (%s) VALUES (", table, fields_buf);
 
-    PGresult *res = PQexec(pg_conn, query);
+    for (int i = 0; i < count; i++) {
+        char param[8];
+        snprintf(param, sizeof(param), "$%d", i + 1);
+        strcat(query, param);
+        if (i < count - 1) {
+            strcat(query, ", ");
+        }
+    }
+    strcat(query, ");");
+
+    // Pole hodnot pro parametry
+    const char *paramValues[count];
+    for (int i = 0; i < count; i++) {
+        paramValues[i] = values[i];
+    }
+
+    // Spustíme parametrizovaný dotaz
+    PGresult *res = PQexecParams(pg_conn,
+                                 query,
+                                 count,       // počet parametrů
+                                 NULL,        // typy (NULL = nechá to na serveru)
+                                 paramValues, // hodnoty parametrů
+                                 NULL,        // délky parametrů (NULL znamená textové)
+                                 NULL,        // formát parametrů (NULL = text)
+                                 0);          // formát výsledku (0 = text)
+
     if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-        kore_log(LOG_ERR, "Insert selhal: %s", PQerrorMessage(pg_conn));
+        kore_log(LOG_ERR, "SQL insert failed: %s", PQerrorMessage(pg_conn));
         PQclear(res);
         return 0;
     }
+
     PQclear(res);
     return 1;
 }
@@ -382,7 +462,12 @@ void model_add_field(db_model_t *model, const char *field_name, const char *fiel
     model->field_types[model->field_count - 1] = strdup(field_type);
 }
 
-db_result_t* model_select(const char *table, const char *where_clause) {
+db_result_t* sql_select(const char *table, const char *where_clause) {
+    if (!is_valid_sql_id(table)) {
+        kore_log(LOG_ERR, "SQL select failed: invalid table id: %s", table);
+        return NULL;
+    }
+
     char query[MAX_QUERY_SIZE];
     if (where_clause) {
         snprintf(query, sizeof(query), "SELECT * FROM %s WHERE %s", table, where_clause);
@@ -392,7 +477,7 @@ db_result_t* model_select(const char *table, const char *where_clause) {
 
     PGresult *res = PQexec(pg_conn, query);
     if (PQresultStatus(res) != PGRES_TUPLES_OK) {
-        kore_log(LOG_ERR, "Dotaz selhal: %s", PQerrorMessage(pg_conn));
+        kore_log(LOG_ERR, "SQL select failed: %s", PQerrorMessage(pg_conn));
         PQclear(res);
         return NULL;
     }
